0be30241b5ff2aa7b675b81926ddcac7cbf67929
Kenny Dao <kennydao369@gmail.com>
2020-09-29 18:07:11 +1000

    adding to remote repo

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "efee2477b4416807e97676367e3ace5c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBUTbFlMTIl7HZKwqiNnuVjhiezzzKq_es"
